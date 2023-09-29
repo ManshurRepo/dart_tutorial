4318a8d0ad26b13d7d20ed858645838e1edc6be3
ManshurRepo <muhammadtm35@gmail.com>
2023-09-29 19:28:14 +0700

    if boolean

--- lib/conditions_loops.dart
+++ lib/conditions_loops.dart
@@ -9,11 +9,20 @@
 } */
 
 //1.2 if-Else conditions
-void main() {
+/*void main() {
   int umur = 30;
   if (umur >= 18) {
     print("you are voter");
   } else {
     print("you are not voter");
+  }
+}*/
+
+void main() {
+  bool isMarried = false;
+  if (isMarried) {
+    print("You are married.");
+  } else {
+    print("You are single.");
   }
 }
