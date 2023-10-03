d73aae0f6c933ac21c5c4051e4ef0b45327a671a
ManshurRepo <muhammadtm35@gmail.com>
2023-10-03 08:16:35 +0700

    latihan soal dart-tutorial

--- lib/condtions_looping.dart
+++ lib/condtions_looping.dart
@@ -272,7 +272,7 @@ void main() {
 }*/
 
 //3. Write a dart program to check whether a number is positive, negative, or zero.
-import 'dart:io';
+/*import 'dart:io';
 
 void main() {
   stdout.write('Masukan angka: ');
@@ -284,5 +284,135 @@ void main() {
     print("$angka angka negatif");
   } else {
     print("$angka angkanya 0");
+  }
+} */
+
+//4. Write a dart program to print your name 100 times.
+/*void main() {
+  int i = 1;
+  while (i <= 100) {
+    print(" $i. Taufiq");
+    i++;
+  }
+} */
+
+//5. Write a dart program to calculate the sum of natural numbers.
+/*void main() {
+  int i = 1;
+  int n = 2;
+  int total = 0;
+  while (i <= n) {
+    total = total + i;
+    i++;
+  }
+  print("totalnya adalah: $total");
+} */
+
+//6. Write a dart program to generate multiplication tables of 5
+/*void main() {
+  //define the number for which you want to generate the multiplcation
+  int number = 5;
+
+  //define the range of multiplication table (contoh dari 1 s/d 10)
+  int start = 1;
+  int end = 10;
+
+  //generate and print the multipilcation table
+  print("Multiplication table of $number");
+  for (int i = start; i <= end; i++) {
+    int result = number * i;
+    print("$number x $i = $result");
+  }
+}*/
+
+//7. Write a dart program to generate multiplication tables of 1-9.
+/*void main() {
+  for (int i = 1; i <= 9; i++) {
+    print("multiplication table for $i is: ");
+    {
+      for (int j = 1; j <= 10; j++) {
+        int result = i * j;
+        print("$i x $j = $result");
+      }
+      print("-------------");
+    }
+  }
+}*/
+
+//8. Write a dart program to generate multiplication tables of 1-9.
+/* import 'dart:io';
+
+void main() {
+  print("""berikut adalah kalkulator sederhana, masukan angka:
+  - 1 untuk pembagian
+  - 2 untuk perkalian
+  - 3 untuk penjumlahan
+  - 4 untuk pengurangan
+  """);
+  stdout.write(
+      "silahkan masukan angka seusai operasi penghitungan yang diingikan: ");
+  int jenisOperasi = int.parse(stdin.readLineSync()!);
+
+  //operasi pembagian
+  if (jenisOperasi == 1) {
+    //masukan angka penyebut
+    stdout.write("masukan angka penyebut: ");
+    int penyebut = int.parse(stdin.readLineSync()!);
+    // masukan angka pembilang
+    stdout.write("masukan angka angka pembilang: ");
+    int pembilang = int.parse(stdin.readLineSync()!);
+    //melakukan operasi penghitungan
+    double result = penyebut / pembilang;
+    print("$penyebut : $pembilang = $result");
+  }
+
+  //operasi perkalian
+  if (jenisOperasi == 2) {
+    //masukan angka penyebut
+    stdout.write("masukan angka penyebut: ");
+    int penyebut = int.parse(stdin.readLineSync()!);
+    // masukan angka pembilang
+    stdout.write("masukan angka angka pembilang: ");
+    int pembilang = int.parse(stdin.readLineSync()!);
+    //melakukan operasi penghitungan
+    int result = penyebut * pembilang;
+    print("$penyebut * $pembilang = $result");
+  }
+
+  //operasi penjumlahan
+  if (jenisOperasi == 3) {
+    //masukan angka penyebut
+    stdout.write("masukan angka penyebut: ");
+    int penyebut = int.parse(stdin.readLineSync()!);
+    // masukan angka pembilang
+    stdout.write("masukan angka angka pembilang: ");
+    int pembilang = int.parse(stdin.readLineSync()!);
+    //melakukan operasi penghitungan
+    int result = penyebut + pembilang;
+    print("$penyebut + $pembilang = $result");
+  }
+
+  //operasi pengurangan:
+  if (jenisOperasi == 4) {
+    //masukan angka penyebut
+    stdout.write("masukan angka penyebut: ");
+    int penyebut = int.parse(stdin.readLineSync()!);
+    // masukan angka pembilang
+    stdout.write("masukan angka angka pembilang: ");
+    int pembilang = int.parse(stdin.readLineSync()!);
+    //melakukan operasi penghitungan
+    int result = penyebut - pembilang;
+    print("$penyebut - $pembilang = $result");
+  }
+}
+*/
+
+//9. Write a dart program to print 1 to 100 but not 41
+void main() {
+  for (int i = 1; i <= 10; i++) {
+    if (i == 5) {
+      continue;
+    }
+    print(i);
   }
 }
